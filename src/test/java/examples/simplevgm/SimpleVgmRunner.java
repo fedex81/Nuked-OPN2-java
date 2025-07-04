3858305770649ab01f32f83f81e43c10bffdc276
Federico Berti <fberti146+git@gmail.com>
2025-07-04 22:52:57 +0100

    add simplevgm example

---
+++ src/test/java/examples/simplevgm/SimpleVgmRunner.java
@@ -0,0 +1,19 @@
+package examples.simplevgm;
+
+import uk.co.omgdrv.simplevgm.Runner;
+import uk.co.omgdrv.simplevgm.VgmEmu;
+
+/**
+ * Federico Berti
+ * <p>
+ * Copyright 2025
+ */
+public class SimpleVgmRunner {
+
+    /**
+     * args: <file>|<folder>`
+     */
+    public static void main(String[] args) throws Exception {
+        Runner.main(args);
+    }
+}
